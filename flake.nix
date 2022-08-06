da4fc3f59a39c0177e64884f63ce46c0b3e6c04a
Denis Bueno <dbueno@gmail.com>
2022-08-06 10:31:51 -0600

    use -Werror for development only

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
             packages = with pkgs; [ creduce ];
             inherit (horn2vmt) cmakeFlags;
             hardeningDisable = [ "all" ];
+            CXXFLAGS = "-Werror";
           };
       });
 }
