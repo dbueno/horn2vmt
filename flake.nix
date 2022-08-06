8f40db3e9cce0266a9e1015dbf8d6902094676f7
Denis Bueno <dbueno@gmail.com>
2022-08-06 10:24:41 -0600

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        horn2vmt = pkgs.callPackage ./default.nix {};
+      in {
+        packages = {
+          inherit horn2vmt;
+        };
+        defaultPackage = horn2vmt;
+        devShell =
+          pkgs.mkShell {
+            inputsFrom = [ horn2vmt ];
+            packages = with pkgs; [ creduce ];
+            inherit (horn2vmt) cmakeFlags;
+            hardeningDisable = [ "all" ];
+          };
+      });
+}
